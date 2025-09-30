c8abbdfd603beff6a76e2485debd705513f4c2d4
Sivasubramanian T <39884769+siva1160github@users.noreply.github.com>
2025-09-30 16:38:50 +0530

    Add files via upload

---
+++ DotnetPracticePbm/DotnetPracticePbm/Problems.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace DotnetPracticePbm
+{
+    public class Problems
+    {
+        public void ReverseString()
+        {
+            Console.WriteLine("Please Enter the String");
+            string input = Console.ReadLine();
+            StringBuilder output = new();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output.Append(input[i]);
+            }
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
---
+++ DotnetPracticePbm/DotnetPracticePbm/Program.cs
@@ -0,0 +1,18 @@
+﻿using DotnetPracticePbm;
+
+static void Functionality()
+{
+    Console.WriteLine("1.ReverseString");
+    int input = int.Parse(Console.ReadLine());
+    Problems problem = new();
+    switch (input)
+    {
+        case 1:
+            problem.ReverseString();
+            break;
+    }
+    Functionality();
+}
+
+Functionality();
+
